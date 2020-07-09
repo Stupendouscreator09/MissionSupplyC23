9398faab685f646fcdfbfc746249b84d73dcf41b
Stupendouscreator09 <66833676+Stupendouscreator09@users.noreply.github.com>
2020-07-09 20:25:53 +0530

    MissionSupplyc23
    When the down arrow is pressed, helicopter drops the box which lands in the red Box.

---
+++ project22/SupplyMission-master/sketch.js
@@ -0,0 +1,92 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var rect1Sprite,rect2Sprite,rect3Sprite;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	rect1Sprite=createSprite(400,640,105,20);
+	rect1Sprite.shapeColor = "red";
+
+  rect2Sprite=createSprite(350,600,20,100);
+  rect2Sprite.shapeColor = "red";
+
+ rect3Sprite=createSprite(450,600,20,100);
+ rect3Sprite.shapeColor = "red";
+
+	
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  packageSprite.x= packageBody.position.x
+  packageSprite.y= packageBody.position.y
+
+drawSprites();
+
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false);
+	packageBody.velocityY = -10;
+	
+	packageSprite.collide(rect1Sprite);
+	packageSprite.collide(rect2Sprite);
+	packageSprite.collide(rect3Sprite);
+	
+
+  
+	
+  }
+}
+
+function keyUP (){
+if (keyCode === UP_ARROW) {
+	Matter.Body.setStatic(packageBody,true);
+
+}
+
+}
